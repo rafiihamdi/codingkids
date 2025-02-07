44d630f3eb53111500a32c57b194341ad0831c5b
rafiihamdi <rafiihamdi58@guru.sd.belajar.id>
2025-02-07 14:53:37 +0800

    file upload koding kids

---
+++ story_content/user.js
@@ -0,0 +1,22 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6XBFWdwsUpv":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSongku');
+audio.src="musikku.mp3";
+
+audio.load();
+
+audio.play();
+
+audio.volume=0.3;
+}
+
